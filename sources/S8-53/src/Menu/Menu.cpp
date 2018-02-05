e1d4b8b3bf3456eca23ae6f7e14803f93270865f
Sasha7b9 <Sasha7b9@tut.by>
2018-02-05 09:25:24 +0300

    Исправлен баг, когда при выборе измерений и только одном измерени горела лампочка установка

--- sources/S8-53/src/Menu/Menu.cpp
+++ sources/S8-53/src/Menu/Menu.cpp
@@ -795,10 +795,10 @@ bool Menu::NeedForFireSetLED()    // Возвращает true, если лампочка УСТАНОВКА до
     NamePage name = GetNameOpenedPage();
 
     if (
-        name == Page_SB_MathCursorsFFT      || 
-        name == Page_SB_MeasTuneMeas        || 
-        name == Page_SB_MemLatest           || 
-        name == Page_SB_MemInt              ||
+        name == Page_SB_MathCursorsFFT                      ||
+        (name == Page_SB_MeasTuneMeas && MEAS_NUM != MN_1)  ||
+        name == Page_SB_MemLatest                           || 
+        name == Page_SB_MemInt                              ||
         (name == Page_SB_MathFunction && !DISABLED_DRAW_MATH) ||
         name == Page_SB_Curs && sCursors_NecessaryDrawCursors()
         )
