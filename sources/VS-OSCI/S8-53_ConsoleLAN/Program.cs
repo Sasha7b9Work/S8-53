3213f0f2c4c3eb7711bfd3855e49112e120f2977
Sasha7b9 <Sasha7b9@tut.by>
2018-04-02 10:38:37 +0300

    Добавлены проекты ConsoleUSB и ConsoleLAN для Visual Studio

---
+++ sources/VS-OSCI/S8-53_ConsoleLAN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S8_53_ConsoleLAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
