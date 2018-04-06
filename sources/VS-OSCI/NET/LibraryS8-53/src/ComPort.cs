36a8fdcf6868ee38d09ac9c1397b36fafa8eaf20
Sasha7b9 <Sasha7b9@tut.by>
2018-04-06 15:01:14 +0300

    Visual Studio Добавлена заготовка для библиотеки компонентов

---
+++ sources/VS-OSCI/NET/LibraryS8-53/src/ComPort.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryS8_53
+{
+    public class ComPort
+    {
+    }
+}
